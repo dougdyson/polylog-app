cccbf39d0d2f38d5a6f11e7e562134481579bc82
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-26 11:15:09 -0700

    remove unused return elements

--- src/components/LectureCard/LectureCard.jsx
+++ src/components/LectureCard/LectureCard.jsx
@@ -11,16 +11,12 @@ function LectureCard(props) {
   // const [lectureDescription, setLectureDescription] = userState(props.lectureDescription || null);
 
   return (
-    // <div>
-      <section className="lectureCard">
-        {/* <div className="lectureCardDetails"> */}
-        <h2>This is a Lecture Card Title</h2>
-        <p>20 Oct/2020 - 9:30am</p>
-        <HistoryIcon />
-        <SettingsIcon />
-        {/* </div> */}
-      </section>
-    // </div>
+    <section className="lectureCard">
+      <h2>This is a Lecture Card Title</h2>
+      <p>20 Oct/2020 - 9:30am</p>
+      <HistoryIcon />
+      <SettingsIcon />
+    </section>
   );
 }
 
