d711e099c83b77fce514279894e5b9f9e185fc50
TheMartonfi <carlmartonfi@hotmail.com>
2020-10-26 19:27:39 -0600

    Make pseudo code for getting session activity and start working on newTopicCard function

--- src/hooks/useTopicCardData.jsx
+++ src/hooks/useTopicCardData.jsx
@@ -3,14 +3,29 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
-const useTopicCardData = lecture_id => {
+const useTopicCardData = (lecture_id, session_uuid = null) => {
 	const [topicCards, setTopicCards] = React.useState([]);
 
 	React.useEffect(() => {
 		axios.get(`/topic/${lecture_id}`).then(res => {
+			// axios
+			// .get(`/topic/response/${session_uuid}`, {
+			//   params: { topic_card_id: 1 }
+			// })
+			// .then(res => {});
+
+			// if (session_uuid)
+			// loop over current quiz cards
+			// axios.get responses for topic_id with session id
+			// add that response to the topic card object with a key of activity
+			// setTopicCards(prev => [...prev, { ...topicCard, activity: ...responses }])
 			setTopicCards([...res.data]);
 		});
 	}, []);
+
+	const newTopicCard = (lecture_id, title, description, position) => {
+		return axios.post(`/topic/card`).then(res => {});
+	};
 
 	return { topicCards };
 };
