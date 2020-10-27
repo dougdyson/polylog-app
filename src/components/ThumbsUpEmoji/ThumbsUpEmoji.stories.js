4abf9fbf09ca5f6222e40274af0fbd9391f22450
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-27 11:54:50 -0700

    add thumbs_up emoji

---
+++ src/components/ThumbsUpEmoji/ThumbsUpEmoji.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ThumbsUpEmoji from './ThumbsUpEmoji';
+
+export default {
+  title: 'Thumbs Up Emoji',
+  component: ThumbsUpEmoji
+}
+
+export const ThumbsUp = () => <ThumbsUpEmoji />
