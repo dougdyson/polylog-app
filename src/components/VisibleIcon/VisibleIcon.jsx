06d96df37c2c375373d6fd8112c09e1dee483388
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-27 11:14:41 -0700

    add visibility icons

---
+++ src/components/VisibleIcon/VisibleIcon.jsx
@@ -0,0 +1,12 @@
+import {ReactComponent as VisibleIcon} from "./visibility-24px.svg";
+import '../../App.css';
+
+function EyeballIcon() {
+  return (
+    <div>
+      <VisibleIcon className="icon"/>
+    </div>
+  )
+}
+
+export default EyeballIcon;
---
+++ src/components/VisibleOffIcon/VisibleOffIcon.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import VisibleOffIcon from './VisibleOffIcon';
+
+export default {
+  title: 'Visible Off Icon',
+  component: VisibleOffIcon
+}
+
+export const EyeballClosedIcon = () => <VisibleOffIcon />
