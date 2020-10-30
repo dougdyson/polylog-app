485eb3bca5796f34d6d7d86ecaddc5916769cf18
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-30 01:31:42 -0700

    add History component skeleton

---
+++ src/components/History/History.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function History(props) {
+  return (
+    <div>
+      <h1>{props.message}</h1>
+    </div>
+  );
+}
+
+export default History;
---
+++ src/components/History/History.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import History from './History';
+
+export default {
+  title: 'History',
+  component: History
+}
+
+export const SessionHistory = () => <History message='Hello!' />
