cc83b1f09fbbe474e2d5e806b7daa4d0f36f9e10
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-30 17:56:32 -0700

    added close button and panel header title

--- src/components/ActivityFeed/ActivityFeed.jsx
+++ src/components/ActivityFeed/ActivityFeed.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import Button from '../Button/Button';
 import LectureInfo from '../LectureInfo/LectureInfo';
 import TopicContainer from '../TopicCard/TopicCard';
+import { ReactComponent as ActivityFeedIcon } from './playlist_add_check-24px.svg';
 
 import './ActivityFeed.css'
 
@@ -8,7 +10,14 @@ import './ActivityFeed.css'
 export default function ActivityFeed(props) {
   
   return (
-    <div className='actitity-feed-container'>
+    <div className='activity-feed-container'>
+      <div className='activity-feed-card-header-row'>
+        <div className='activity-feed-card-header'>
+          <ActivityFeedIcon className='activity-feed-icon' />
+          <h2 className='activity-feed-card-title'>Lecture Activity Feed</h2>
+        </div>
+        <Button variant='close'>x</Button>
+      </div>
       <LectureInfo />
       <TopicContainer />
     </div>
