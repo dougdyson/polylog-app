16f0fd5d29ab311a2a84f2f3434d46afb43cf5cc
Unfamous Dyson <hatheadninja@gmail.com>
2020-11-01 10:32:57 -0800

    fix typo bug

--- src/components/Quiz/QuizCard.jsx
+++ src/components/Quiz/QuizCard.jsx
@@ -32,7 +32,7 @@ export default function Card(props) {
       <div>
         <Answer />
         <Message />
-&        
+       
         { (isLecturer)
             ? <div>
                 <CorrectAnswerCount />
