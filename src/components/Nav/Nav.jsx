4134e763c8461bdf50a16412ba94c73d059aba00
TheMartonfi <carlmartonfi@hotmail.com>
2020-11-03 14:06:09 -0700

    Make lectures link in nav bar and make it look like user is logged in

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,32 @@
-import './Nav.css';
-import {ReactComponent as Logo} from "./polylog-logo.svg";
-import Button from '../Button/Button';
+import "./Nav.css";
+import { ReactComponent as Logo } from "./polylog-logo.svg";
+import { Link } from "react-router-dom";
+import Button from "../Button/Button";
 import "fontsource-roboto";
 
-export default function Nav () {
+export default function Nav() {
+	return (
+		<section className="nav-top">
+			<div className="nav-top-left">
+				<a href="#">
+					<Logo />
+				</a>
+				<div className="nav-top-menu-item-left">
+					<a href="#">team</a>
+				</div>
+				<div className="nav-top-menu-item-left">
+					<a href="#">coming soon</a>
+				</div>
+			</div>
 
-  return (
-    <section className='nav-top'>
-      <div className='nav-top-left'>
-        <a href='#'><Logo /></a>
-        <div className='nav-top-menu-item-left'><a href='#'>team</a></div>
-        <div className='nav-top-menu-item-left'><a href='#'>coming soon</a></div>
-      </div>
-
-      <div className='nav-top-right'>
-        <div className='nav-top-menu-item-right'><a href='#'>login</a></div>
-        <div className='nav-top-menu-item-right'>
-          <Button variant='nav-bar'>SIGN UP</Button>
-        </div>
-      </div>
-    </section>
-  )
+			<div className="nav-top-right">
+				<div className="nav-top-menu-item-right">
+					<a href="/lectures">lectures</a>
+				</div>
+				<div className="nav-top-menu-item-right">
+					<Button variant="nav-bar">logout</Button>
+				</div>
+			</div>
+		</section>
+	);
 }
